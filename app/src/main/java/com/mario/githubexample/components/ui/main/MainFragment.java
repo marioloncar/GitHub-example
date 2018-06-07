5c1dc8c571d6eb02a771d13064167c8787f5d51d
Mario <loncar.mario1@gmail.com>
2018-06-07 12:11:56 +0200

    Create recyclerview search item

--- app/src/main/java/com/mario/githubexample/components/ui/main/MainFragment.java
+++ app/src/main/java/com/mario/githubexample/components/ui/main/MainFragment.java
@@ -2,12 +2,19 @@ package com.mario.githubexample.components.ui.main;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.mario.githubexample.R;
 import com.mario.githubexample.components.base.BaseDialogFragment;
 import com.mario.githubexample.components.di.ActivityScoped;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
 
 /**
  * Created by mario on 06/06/18.
@@ -23,6 +30,11 @@ public class MainFragment extends BaseDialogFragment<MainContract.Presenter> imp
     public MainFragment() {
     }
 
+    @BindView(R.id.edittext_search)
+    EditText editTextSearch;
+    @BindView(R.id.recyclerview_search_results)
+    RecyclerView recyclerViewResults;
+
     @Override
     protected MainContract.Presenter getPresenter() {
         return presenter;
@@ -37,5 +49,21 @@ public class MainFragment extends BaseDialogFragment<MainContract.Presenter> imp
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         presenter.setView(this);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.options_item_filter, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_filter:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
